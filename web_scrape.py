975e0b0d84c75d29c1cc4b7e553d5dd7724eec6c
DarkkLegion <arshad18syed@gmail.com>
2023-03-28 21:15:11 +0530

    Initial commit

---
+++ web_scrape.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from urllib.parse import urljoin
+
+# Set the base URL
+base_url = 'https://novelfull.com/'
+
+# Set the URL of the first chapter
+url = urljoin(base_url, 'hidden-marriage/chapter-2164-my-heart-belongs-to-you.html')
+
+# Initialize a list to store the chapter dictionaries
+chapters = []
+
+# Loop to scrape all chapters
+while True:
+    # Send an HTTP request to the URL and get the HTML content
+    response = requests.get(url)
+    html_content = response.content
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find the story text element and get its contents
+    story_text = soup.find('div', {'id': 'chapter-content'})
+    chapter_content = '\n'.join([p.text for p in story_text.find_all('p')])
+
+    # Add the chapter dictionary to the list
+    chapter_number = url.split('/')[-1].split('.')[0].split('-')[-1]
+    chapter_dict = {'number': chapter_number, 'content': chapter_content}
+    chapters.append(chapter_dict)
+
+    # Find the "Next Chapter" button element and extract the URL of the next chapter
+    next_chapter_button = soup.find('a', {'id': 'next_chap'})
+    try:
+        if next_chapter_button:
+            next_chapter_url = next_chapter_button['href']
+            url = urljoin(base_url, next_chapter_url)
+        else:
+            print("\n\nEnd")
+            break  # End the loop if there are no more chapters
+    except KeyError:
+        print(next_chapter_button)
+    """
+    finally:
+        with open('C:/Users/arsha/OneDrive/Desktop/hidden-marriage.json', 'w') as f:
+            json.dump(chapters, f)
+
+
+# Save the chapters list to a JSON file
+with open('C:/Users/arsha/OneDrive/Desktop/hidden-marriage.json', 'w') as f:
+    json.dump(chapters, f)
+    """
